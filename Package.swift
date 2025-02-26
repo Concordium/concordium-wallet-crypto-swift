96f6dd13c8b6515bd12b4936db54ffd2ec112746
Max Rachytskyy <makr@concordium.com>
2025-02-26 11:30:22 +0200

    update ckechsum

--- Package.swift
+++ Package.swift
@@ -11,6 +11,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "libmobile_wallet", url: "https://s3.eu-west-1.amazonaws.com/static-libraries.concordium.com/iOS/libmobile_wallet_0.25.0.xcframework.zip", checksum: "1b3155390cff94eaccd4230ecbdc5a081ead505c3ccdaf0b87a2279f1b05dfc8")
+        .binaryTarget(name: "libmobile_wallet", url: "https://s3.eu-west-1.amazonaws.com/static-libraries.concordium.com/iOS/libmobile_wallet_0.25.0.xcframework.zip", checksum: "c16289ce5eab7e4990afea12f806fb892f0621e39ab19126fe402631553cb3aa")
     ]
 )
